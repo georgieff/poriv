ee5ea124d42f60361076a1d1138a46f65b4b52bd
georgieff <lubo@georgieff.it>
2014-02-03 00:17:31 +0200

    basic assets set up

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>poriv</title>
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+  <div class="cont">
+    <div class="row">
+      <div class="col6" style="background: #444">asd</div>
+      <div class="col6" style="background: #444">asd</div>
+    </div>
+  </div>
+</body>
+</html>
