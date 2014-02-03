607431dda89f3bd88be951a196d02a84e5cc1c93
georgieff <lubo@georgieff.it>
2014-02-04 00:47:34 +0200

    added fonts properties

--- index.php
+++ index.php
@@ -3,12 +3,16 @@
 <head>
   <meta charset="UTF-8">
   <title>poriv</title>
+  <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:300&subset=latin,cyrillic-ext,cyrillic' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Roboto:700,300&subset=latin,cyrillic-ext,cyrillic' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
   <div class="cont">
     <div class="row">
-      <div class="col6" style="background: #444">asd</div>
+      <div class="col6" style="background: #444">
+      <h1>Тест</h1>
+      </div>
       <div class="col6" style="background: #444">asd</div>
     </div>
   </div>
