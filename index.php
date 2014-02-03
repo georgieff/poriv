4c3ae820cfe61f4653db4c112119b9c41b1482e1
georgieff <lubo@georgieff.it>
2014-02-04 01:46:04 +0200

    added some more section styles and content

--- index.php
+++ index.php
@@ -7,14 +7,59 @@
   <link href='http://fonts.googleapis.com/css?family=Roboto:700,300&subset=latin,cyrillic-ext,cyrillic' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="assets/css/style.css">
 </head>
-<body>
-  <div class="cont">
-    <div class="row">
-      <div class="col6" style="background: #444">
-      <h1>Тест</h1>
+<body id="mod">
+  <section class="Section-nav">
+    <div class="cont">
+      <div class="row">
+        <div class="col1 logo">
+          <img src="assets/img/poriv-logo.png" alt="poriv">
+        </div>
+        <div class="col11">
+          <ul class="Nav">
+            <li><a href="">За Проекта</a></li>
+            <li><a href="">Партньори</a></li>
+            <li><a href="">Новини</a></li>
+            <li><a href="">Метод</a></li>
+            <li><a href="">Добри Практики</a></li>
+            <li><a href="">Документи</a></li>
+            <li><a href="">Галерия</a></li>
+            <li><a href="">Контакти</a></li>
+            <li><a href="" class="special">En</a></li>
+          </ul>
+        </div>
       </div>
-      <div class="col6" style="background: #444">asd</div>
     </div>
-  </div>
+  </section>
+
+  <section class="Section-eu mtac">
+    <div class="cont">
+      <div class="row">
+        <div class="col1">
+          <img src="assets/img/eu.png" alt="EU">
+        </div>
+        <div class="col10" >
+          <span>ПОРИВ (подкрепа за равни възможности) Договор BG 051PO001-7.0.07-0247-C0001 Схема BG 051PO001-7.0.07
+            <br>Без граници – Компонент 1 – Фаза 2
+          </span>
+        </div>
+        <div class="col1">
+          <img src="assets/img/esf.png" alt="ESF">
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="Section-header">
+    <div class="cont">
+      <div class="row">
+        <div class="col12">
+          <h1 class="large">Развите на културни<br>маршрути</h1>
+          <h5>Метод за стимулиране на<br> социалната активност и местното<br> развитие</h5>
+        </div>
+      </div>
+    </div>
+  </section>
+
 </body>
 </html>
