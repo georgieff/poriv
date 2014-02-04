03bc60fd90f426fdd06e9ebdc863d90913783c0e
georgieff <lubo@georgieff.it>
2014-02-04 21:31:06 +0200

    home page finished

--- index.php
+++ index.php
@@ -11,10 +11,10 @@
   <section class="Section-nav">
     <div class="cont">
       <div class="row">
-        <div class="col1 logo">
+        <div class="col1 logo mtar">
           <img src="assets/img/poriv-logo.png" alt="poriv">
         </div>
-        <div class="col11">
+        <div class="col11 mtar">
           <ul class="Nav">
             <li><a href="">За Проекта</a></li>
             <li><a href="">Партньори</a></li>
@@ -34,28 +34,123 @@
   <section class="Section-eu mtac">
     <div class="cont">
       <div class="row">
-        <div class="col1">
+        <div class="col2 mtal">
           <img src="assets/img/eu.png" alt="EU">
         </div>
-        <div class="col10" >
+        <div class="col8 mdib mmt1" >
           <span>ПОРИВ (подкрепа за равни възможности) Договор BG 051PO001-7.0.07-0247-C0001 Схема BG 051PO001-7.0.07
             <br>Без граници – Компонент 1 – Фаза 2
           </span>
         </div>
-        <div class="col1">
+        <div class="col2">
           <img src="assets/img/esf.png" alt="ESF">
         </div>
       </div>
     </div>
   </section>
 
-
   <section class="Section-header">
     <div class="cont">
       <div class="row">
         <div class="col12">
-          <h1 class="large">Развите на културни<br>маршрути</h1>
+          <h1 class="large">Развитие на културни<br>маршрути</h1>
           <h5>Метод за стимулиране на<br> социалната активност и местното<br> развитие</h5>
+          <a href="#" class="Button Button-seeMore mmb3">Научи повече</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="Section-big">
+    <div class="cont">
+      <div class="row">
+        <div class="col12">
+          <h3>
+            Проучване и прилагане опита на Фондация
+            “Образование за демокрация” за разработване на културни маршрути в област Кюстендил
+          </h3>
+          <hr>
+        </div>
+        <div class="col4 NewsBlock">
+          <h2>Последни новини</h2>
+          <a href="news">Виж Всички  &#8594;</a>
+          <hr>
+        </div>
+        <div class="col4 NewsBlock">
+          <span class="Number">07.05</span>
+          <a class="h5 mnm" href="#">Стартира изпълнението на проект ПОРИВ</a>
+          <hr>
+        </div>
+        <div class="col4 NewsBlock">
+          <span class="Number">12.08</span>
+          <a class="h5 mnm" href="#">На пресконференция в гр. Кюстендил беше представен проект ПОРИВ</a>
+          <hr>
+        </div>
+        <div class="col4 NewsBlock">
+          <span class="Number">19.04</span>
+          <a class="h5 mnm" href="#">Стартира изпълнението на проект ПОРИВ </a>
+        </div>
+        <div class="col4 NewsBlock">
+          <span class="Number">31.12</span>
+          <a class="h5 mnm" href="#">Създаването на нови маршрути започна във вторник в 12.00 ч.</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="Section-grey">
+    <div class="cont">
+      <div class="row">
+        <div class="col10">
+          <h2>Галерия </h2>
+        </div>
+        <div class="col2 mtar">
+          <a href="#" class="ShowAll">All</a>
+          <a href="#" class="ShowPrev">Previous</a>
+          <a href="#" class="ShowNext">Next</a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col4">
+          <img src="assets/img/gallery1.png" alt="">
+        </div>
+        <div class="col4">
+          <img src="assets/img/gallery2.png" alt="">
+        </div>
+        <div class="col4">
+          <img src="assets/img/gallery3.png" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="Section-black">
+    <div class="cont">
+      <div class="row">
+        <div class="col12">
+          <ul class="Nav">
+            <li><a href="">За Проекта</a></li>
+            <li><a href="">Партньори</a></li>
+            <li><a href="">Новини</a></li>
+            <li><a href="">Метод</a></li>
+            <li><a href="">Добри Практики</a></li>
+            <li><a href="">Документи</a></li>
+            <li><a href="">Галерия</a></li>
+            <li><a href="">Контакти</a></li>
+          </ul>
+          <hr>
+        </div>
+        <div class="col12">
+          <h6 class="mmb1">Порив</h6>
+        </div>
+        <div class="col9">
+          <span class="Small">
+            Този сайт е изготвен с финансовата помощ на Европейският социален фонд. Фондация „Гражданска инициатива за местно развитие” носи цялата отговорност за съдържанието на настоящият сайт и при никакви обстоятелства не моце да се проеме като официална позиция на Еврпейския съюз или МТСП и главна дирекция „Европейски фондове, международни програми и проекти”.
+          </span>
+        </div>
+        <div class="col3">
+        <span class="Phone mdb mmb1">GSM: 0893 522 519</span>
+        <span>E-mail:</span> <a href="maito:gimr211@abv.bg">gimr211@abv.bg</a>
         </div>
       </div>
     </div>
